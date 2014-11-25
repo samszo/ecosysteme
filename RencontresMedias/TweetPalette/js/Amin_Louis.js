c6f04c519dba7fe72583bb910d44b2941edd3b99
LouisCrolus <louiscrolus@gmail.com>
2014-11-25 21:56:30 +0100

    modifs

---
+++ RencontresMedias/TweetPalette/js/Amin_Louis.js
@@ -0,0 +1,26 @@
+var largeurTampon = 400;
+
+function PageChargee(){
+
+	d3.select("#Calque_2").on("click",AugmenterBarre);
+
+	d3.select("#Calque_3").on("click",DiminuerBarre);
+
+	d3.select("#barre").transition().duration(1000).attr("x2", largeurTampon);
+}
+
+
+function AugmenterBarre(){
+	//alert("ok");
+
+	d3.select("#barre").transition().duration(1000).attr("x2", largeurTampon+40);
+
+	largeurTampon += 40;
+}
+
+function DiminuerBarre(){
+
+	d3.select("#barre").transition().duration(1000).attr("x2", largeurTampon-40);
+
+	largeurTampon -= 40;
+}
