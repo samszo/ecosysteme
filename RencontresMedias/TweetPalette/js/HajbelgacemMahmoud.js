5543b23fc86a1c31eb2f2b2ff7dc85dd41088df4
mahmoudHB <m.hajbelgacem@hotmail.com>
2014-12-01 14:27:47 +0100

    Tweet_Palette

---
+++ RencontresMedias/TweetPalette/js/HajbelgacemMahmoud.js
@@ -0,0 +1,100 @@
+console.log('on y est');
+var clics=[];
+
+var elInt = [
+             {"idEle":"E10","couleur":"#0365f7","hauteur":0}
+             ,{"idEle":"E9","couleur":"#03e3f7","hauteur":0}
+             ,{"idEle":"E8","couleur":"#03f7aa","hauteur":0}
+			 ,{"idEle":"E7","couleur":"#03f781","hauteur":0}
+			,{"idEle":"E6","couleur":"#1ff20c","hauteur":0}
+			,{"idEle":"E5","couleur":"#031ff7","hauteur":0}
+			,{"idEle":"E4","couleur":"#3c03f7","hauteur":0}
+			,{"idEle":"E3","couleur":"#be03f7","hauteur":0}
+		    ,{"idEle":"E2","couleur":"#f7035c","hauteur":0}
+			,{"idEle":"E1","couleur":"#f70303","hauteur":0}
+             ];
+var x;
+
+function ajoutEvent(){
+	
+	elInt.forEach(function(e){
+		ele = document.getElementById(e.idEle);
+		ele.setAttribute("class",'imgEvent');
+	});
+	d3.selectAll(".imgEvent")
+		.data(elInt)
+		.on("click",ajoutClic);
+	
+	x = d3.scale.linear()
+	.domain([0, 1000])
+	.range([0, 800]);
+
+}
+
+function ajoutClic(d, i){
+	d.hauteur += 10;
+	clics.push({"couleur":d.couleur, "hauteur":d.hauteur});
+	updateGraphBar(d);
+}
+
+
+function updateGraphBar(d){
+
+	//d�finit une marge � gauche
+	var margeGauche = 0;
+
+	//d�finit la largeur des barres du graphe
+	var largeur = (800 - margeGauche) / clics.length;
+
+	//cr�e une barre pour la nouvelle temp�rature
+	d3.select("svg")
+		.append("rect")
+		.attr("id", "bar" + clics.length)
+		.attr("y", 600 - d.hauteur/3 - 10)
+		.attr("x", 800)
+		.attr("height", d.hauteur/3 + 10)
+		.attr("width", 0)
+		.style("fill", d.couleur);
+
+	//modifie la largeur et la position de chaque barre du graphe
+	for(var i = 0; i <= clics.length; i++){
+		d3.select("#bar"+i)
+			.transition()
+			.duration(1500)
+			.attr("width", largeur)
+			.attr("x", margeGauche + 1 + largeur * (i-1));
+	}
+}
+
+function updateGraphBarD3(){
+
+	//d�finit une marge � gauche
+	var margeGauche = 200;
+
+	//d�finit la largeur des barres du graphe
+	var largeur = (800 - margeGauche) / clics.length;
+
+	//cr�e une barre pour la nouvelle temp�rature
+	d3.select("svg")
+		.selectAll(".bar")
+		.data(clics).enter()
+		.append("rect")
+		.attr("id", function(d,i){
+			"bar" + i;
+			})
+		.attr("class", "bar")
+		.attr("y", function(d,i){
+			return 600 - d.hauteur/3 - 10;
+		})
+		.attr("x", function(d,i){
+			return 800 - margeGauche * (i+1);
+		})
+		.attr("height", function(d,i){
+			return x(d.hauteur);
+		})
+		.attr("width", function(d) { return x(d.hauteur); })
+		.style("fill", function(d,i){
+			return d.couleur;
+		});
+
+}
